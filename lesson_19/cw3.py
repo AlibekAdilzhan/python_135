6a856a03d7b216330d70cf625a6a9b1f4949427b
AlibekAdilzhan <alibek.adilzhanov@gmail.com>
2022-10-05 23:27:14 +0600

    modules 2

--- lesson_19/cw3.py
+++ lesson_19/cw3.py
@@ -1,5 +1,9 @@
 import my_math
 
-n = 5
-s = my_math.factorial(n)
-print(s)
+
+a = 5
+n = 7
+s = my_math.derivative(a, n)
+random_list = my_math.create_random_list(10)
+
+print(random_list)
--- lesson_19/my_math.py
+++ lesson_19/my_math.py
@@ -1,3 +1,6 @@
+import random
+
+
 def factorial(n):
     s = 1
     for i in range(1, n + 1):
@@ -12,3 +15,21 @@ def circle_area(r):
 
 def derivative(a, n): # a^n -> n*a^(n-1)
     return n * a**(n - 1)
+
+
+def create_random_list(n):
+    a = []
+    for i in range(n):
+        rv = random.randint(-100, 100)
+        a.append(rv)
+    return a
+
+
+HEIGHT = 520
+WIDTH = 800
+COLOR = (120, 0, 0)
+
+hero = {"height" : 180, "age": 28, "name": "Groznyi"}
+
+if __name__ == "__main__":
+    print("hi hi hi")

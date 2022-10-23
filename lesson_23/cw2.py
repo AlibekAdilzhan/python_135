828e697cccef6aa7889f7204f12a40ae3a148ff5
AlibekAdilzhan <alibek.adilzhanov@gmail.com>
2022-10-23 11:26:14 +0600

    lesson 22_2

--- lesson_23/cw2.py
+++ lesson_23/cw2.py
@@ -1,10 +1,15 @@
-# pep-8
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
-def fn_1(x):
-    s = x + 5
-    print(s)
+    def dist(self, point_2):
+        d = ((point_2.x - self.x)**2 + (point_2.y - self.y)**2)**0.5
+        return d
 
 
-def fn_2(x):
-    s = x - 5
-    print(s)
+point_1 = Point(0, 0)
+point_2 = Point(3, 4)
+
+d = point_1.dist(point_2)
+print(d)
